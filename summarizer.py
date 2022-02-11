9f1fa6a0fb80396ac2711b4b6bfa58f3da520876
nateshep <nateshep@localhost.localdomain>
2022-02-11 03:21:58 -0500

    Initial commit

---
+++ summarizer.py
@@ -0,0 +1,18 @@
+from gensim.summarization.summarizer import summarize
+
+print('''
+	summarize(text, ratio=0.2, word_count=None, split=False)
+        Get a summarized version of the given text.
+        
+        The output summary will consist of the most representative sentences
+        and will be returned as a string, divided by newlines.
+''')
+
+text = ""
+filename = "stats406/hw11/fowler_hall_2018.txt"
+with open(filename, 'r') as textfile:
+        text = textfile.read()
+
+ratio = float(input("Summary ratio: "))
+for line in summarize(text, ratio=ratio, split=True):
+	print(line)
